30a78acff8e5a47cde0fef4e9389382186c4949e
JorgeWong6 <Konstrictor65@gmail.com>
2017-10-29 16:38:57 -0700

    first commit

---
+++ app/src/main/java/com/example/jorge/holamundo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.jorge.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv_hw);
+        tv.setText("Este es un texto para simplificar la conexion de JAVA y XML");
+
+    }
+}
